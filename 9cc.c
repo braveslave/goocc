e4555db5e2dc2cf262b8cc07111860f6aef0e384
ao <aolcq8@gmail.com>
2024-07-11 01:37:52 +0900

    構文解析できるようになった

--- 9cc.c
+++ 9cc.c
@@ -4,26 +4,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "header.h"
 
-typedef enum{ 
-    TK_RESERVED,
-    TK_NUM,
-    TK_EOF,
-} TokenKind;
 
-typedef struct Token Token;
+int main(int argc, char **argv){
+    if(argc != 2){
+        fprintf(stderr, "引数の個数が正しくありません\n");
+        return 1;
+    }
 
-struct Token{
-    TokenKind kind;
-    Token *next;
-    int val;
-    char *str;
-};
+    user_input = argv[1];
+    token = tokenize(user_input);
+    Node *node = expr();
 
-Token *token; //?ポインタで持つとなんかうれしいのかわからん -> 連結リスト型っぽいわ普通に
+    printf(".intel_syntax noprefix\n");
+    printf(".globl main\n");
+    printf("main:\n");
 
+    gen(node);
+
+    printf("    pop rax\n");
+    printf("    ret\n"); 
+    return 0;
+}
+
+// val declaration
+
+Token *token; //なんかこっちでも宣言しないと未定義で怒られる泣
 char *user_input;
 
+// func declaration
 void error_at(char *loc, char *fmt, ...){
     va_list ap;
     va_start(ap, fmt);
@@ -82,7 +92,7 @@ Token *tokenize(char *p){
             continue;
         }
 
-        if(*p == '+' || *p == '-'){
+        if(*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')') {
             cur = new_token(TK_RESERVED, cur, p++);
             continue;
         }
@@ -100,31 +110,81 @@ Token *tokenize(char *p){
     return head.next;
 }
 
-int main(int argc, char **argv){
-    if(argc != 2){
-        fprintf(stderr, "引数の個数が正しくありません\n");
-        return 1;
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs){
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_node_num(int val){
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+}
+
+Node *expr(){
+    Node *node = mul();
+    for(;;){
+        if(consume('+'))
+            node = new_node(ND_ADD, node, mul());
+        else if (consume('-'))
+            node = new_node(ND_SUB, node, mul());
+        else
+            return node;
+    }
+}
+
+Node *mul(){
+    Node *node = primary();
+    for(;;){
+        if(consume('*'))
+            node = new_node(ND_MUL, node, primary());
+        else if (consume('/'))
+            node = new_node(ND_DIV, node, primary());
+        else
+            return node;
+    }
+}
+
+Node *primary(){
+    if (consume('(')){
+        Node *node = expr();
+        expect(')');
+        return node;
+    }
+    return new_node_num(expect_number());
+}
+
+void gen(Node *node){
+    if(node->kind == ND_NUM){
+        printf(" push %d\n", node->val);
+        return;
     }
 
-    user_input = argv[1];
-    token = tokenize(user_input);
+    gen(node->lhs);
+    gen(node->rhs);
 
-    printf(".intel_syntax noprefix\n");
-    printf(".globl main\n");
-    printf("main:\n");
-    printf("    mov rax, %d\n", expect_number()); 
+    printf("    pop rdi\n");
+    printf("    pop rax\n");
 
-    while(!at_eof()){
-        if(consume('+')){
-            printf("    add rax, %d\n", expect_number());
-            continue;
-        }
-
-        expect('-');
-        printf("    sub rax, %d\n", expect_number());
-
+    switch (node->kind){
+    case ND_ADD:
+        printf("    add rax, rdi\n");
+        break;
+    case ND_SUB:
+        printf("    sub rax, rdi\n");
+        break;
+    case ND_MUL:
+        printf("    imul rax, rdi\n");
+        break;
+    case ND_DIV:
+        printf("    cqo\n");
+        printf("    idiv rdi\n");
+        break;
     }
 
-    printf("    ret\n"); 
-    return 0;
+    printf("    push rax\n");
 }
---
+++ header.h
@@ -0,0 +1,60 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+// enum
+typedef enum{ 
+    TK_RESERVED,
+    TK_NUM,
+    TK_EOF,
+} TokenKind;
+
+typedef enum{
+    ND_ADD,
+    ND_SUB,
+    ND_MUL,
+    ND_DIV,
+    ND_NUM,
+} NodeKind;
+
+// struct
+typedef struct Token Token; //これ書いとかないとToken内で未定義で怒られる
+typedef struct Node Node;
+
+struct Token{
+    TokenKind kind;
+    Token *next;
+    int val;
+    char *str;
+};
+
+struct Node{
+    NodeKind kind;
+    Node *lhs;
+    Node *rhs;
+    int val;
+};
+
+// global val
+extern Token *token; 
+extern char *user_input;
+
+// func
+// Tokenを触るやつら
+void error_at(char *loc, char *fmt, ...);
+bool consume(char op);
+void expect(char op);
+int expect_number();
+bool at_eof();
+Token *new_token(TokenKind kind, Token *cur, char *str);
+Token *tokenize(char *p);
+
+// Nodeを触るやつら
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs);
+Node *new_node_num(int val);
+Node *expr();
+Node *mul();
+Node *primary();
+void gen(Node *node);
+
+
+#endif
